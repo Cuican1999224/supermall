873975cdbaba84445d85111aa04cb4ba0af39f55
崔灿 <1562031979@qq.com>
2021-01-12 09:03:34 +0800

    提交

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -1,9 +1,4 @@
 <template>
-  <div class="nav-bar">
-    <div class="left"><slot name="left"></slot></div>
-    <div class="center"><slot name="center"></slot></div>
-    <div class="right"><slot name="right"></slot></div>
-  </div>
 </template>
 
 <script>
@@ -13,19 +8,4 @@
 </script>
 
 <style scoped>
-  .nav-bar {
-    display: flex;
-    height: 44px;
-    line-height: 44px;
-    text-align: center;
-    box-shadow: 0 1px 1px rgba(100,100,100,.1);
-  }
-
-  .left, .right {
-    width: 60px;
-  }
-
-  .center {
-    flex: 1;
-  }
 </style>
